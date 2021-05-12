2d777579aa50ea5bcc672490d84328a72f6d00a7
daiane_b <daiane.barbosa@demarco.com.br>
2021-05-12 19:05:04 -0300

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL, {
       useNewUrlParser: true,
-    })
+    }),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
