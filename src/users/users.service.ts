8e4ae7e91440b6790cda964cfc3069d40bf6ae1f
daiane_b <daiane.barbosa@demarco.com.br>
2021-05-28 20:55:54 -0300

    feat: add service to create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateUsersDTO } from './dto/create-users.dto';
 import { Users } from './interfaces/users.interface'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel('Users') private readonly model: Model<Users>) { }
     
-    async findOne(username: string): Promise<Users | undefined> {
-        return this.model.findOne({ username })
+    async findOne(email: string): Promise<Users | undefined> {
+        return this.model.findOne({ email })
+    }
+
+    async create(user: CreateUsersDTO): Promise<Users>{
+        const saltOrRounds = 10;
+        const newUser = new this.model(user);
+        const password = newUser.password;
+        newUser.password = await bcrypt.hash(password, saltOrRounds)
+        console.log({newUser})
+        return newUser.save();
     }
 }
